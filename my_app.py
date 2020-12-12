efb3f8920a67afd1945f54d2711d6717aa7caffa
r0cd7b <tkdrbs1928@naver.com>
2020-12-13 02:10:18 +0900

    ui 변경.

--- my_app.py
+++ my_app.py
@@ -3,12 +3,17 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 import time
 import os
-from picamera import PiCamera
+
+
+# from picamera import PiCamera
 
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.train_label = QLabel('Enter class name:')
+        self.train_edit = QLineEdit(self)
 
         self.capture_train_button = QPushButton("Capture training image")
         self.capture_train_button.clicked.connect(self.capture_train)
@@ -32,12 +37,14 @@ class MyApp(QWidget):
         self.grid = QGridLayout()
         self.setLayout(self.grid)
 
-        self.grid.addWidget(self.capture_train_button, 0, 0)
-        self.grid.addWidget(self.train_button, 1, 0)
-        self.grid.addWidget(self.capture_predict_button, 2, 0)
-        self.grid.addWidget(self.predict_button, 3, 0)
-        self.grid.addWidget(self.browser, 4, 0)
-        self.grid.addWidget(self.clear_button, 5, 0)
+        self.grid.addWidget(self.train_label, 0, 0)
+        self.grid.addWidget(self.train_edit, 1, 0)
+        self.grid.addWidget(self.capture_train_button, 2, 0)
+        self.grid.addWidget(self.train_button, 3, 0)
+        self.grid.addWidget(self.capture_predict_button, 4, 0)
+        self.grid.addWidget(self.predict_button, 5, 0)
+        self.grid.addWidget(self.browser, 6, 0)
+        self.grid.addWidget(self.clear_button, 7, 0)
 
         self.setWindowTitle('Handwriting Similarity Analysis')
         self.setWindowIcon(QIcon('handwriting_icon.png'))
@@ -47,9 +54,9 @@ class MyApp(QWidget):
 
         self.cnn = CNN(self.browser, "train")
 
-        self.camera = PiCamera()
-        self.camera.resolution = (640, 480)
-        self.camera.start_preview(fullscreen=False, window=(100, 100, 640, 480))
+        # self.camera = PiCamera()
+        # self.camera.resolution = (640, 480)
+        # self.camera.start_preview(fullscreen=False, window=(100, 100, 640, 480))
 
     def center(self):
         qr = self.frameGeometry()
@@ -58,11 +65,19 @@ class MyApp(QWidget):
         self.move(qr.topLeft())
 
     def capture_train(self):
-        text, ok = QInputDialog.getText(self, 'Input Owner Name', 'Please enter the owner name of the handwriting:')
-        if ok:
-            image_dir = f"train/{str(text).replace(' ', '_')}/{time.time()}.png"
-            self.camera.capture(image_dir)
-            self.browser.append(f"The image was saved in {image_dir}")
+        text = self.train_edit.text()
+        if text == "":
+            QMessageBox.question(
+                self,
+                'The class name is empty',
+                'Please write the class name',
+                QMessageBox.Yes,
+                QMessageBox.Yes
+            )
+            return
+        image_dir = f"train/{text.replace(' ', '_')}/{time.time()}.png"
+        # self.camera.capture(image_dir)
+        self.browser.append(f"The image was saved in {image_dir}")
 
     def train(self):
         if os.path.exists("model.h5"):
@@ -71,7 +86,7 @@ class MyApp(QWidget):
 
     def capture_predict(self):
         image_dir = f"predict/{time.time()}.png"
-        self.camera.capture(image_dir)
+        # self.camera.capture(image_dir)
         self.browser.append(f"The image was saved in {image_dir}")
 
     def predict(self):
